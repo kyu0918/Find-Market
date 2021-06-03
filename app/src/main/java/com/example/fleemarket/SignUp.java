d4422e5dca68e7372812e3645bb4c4de3adb2f17
kyu0918 <58536057+kyu0918@users.noreply.github.com>
2021-06-03 20:48:33 +0900

    회원가입시 유저 이미지 가져오기

--- app/src/main/java/com/example/fleemarket/SignUp.java
+++ app/src/main/java/com/example/fleemarket/SignUp.java
@@ -1,18 +1,49 @@
 package com.example.fleemarket;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
+
+    private  final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        imageview=(ImageView)findViewById(R.id.user_image);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+                startActivityForResult(intent,GET_GALLERY_IMAGE);
+            }
+        });
+
         ActionBar ab = getSupportActionBar();
         ab.hide();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null){
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
     }
 }
