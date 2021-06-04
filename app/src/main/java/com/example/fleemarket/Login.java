74cbb0d20756c6c82c4d900b5a9d299e6120d9a9
tjdydwns123 <63245488+tjdydwns123@users.noreply.github.com>
2021-06-04 14:26:56 +0900

    회원가입 Java, 로그인 Java, 회원가입 XML Fierbase 동기화 완료

--- app/src/main/java/com/example/fleemarket/Login.java
+++ app/src/main/java/com/example/fleemarket/Login.java
@@ -1,18 +1,30 @@
 package com.example.fleemarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     ImageButton login;
 
+    private FirebaseAuth mAuth;
+    private static final String TAG = "Loding";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,52 @@ public class Login extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.hide();
 
+        mAuth = FirebaseAuth.getInstance();
+
         login = (ImageButton)findViewById(R.id.login1);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainFrame.class);
-                startActivity(intent);
+                Login();
             }
         });
+    } //onCreat()
+
+    private void Login() {
+        String email = ((EditText) findViewById(R.id.edtId)).getText().toString();
+        String password = ((EditText) findViewById(R.id.edtPw)).getText().toString();
+
+        if (email.length() > 0 && password.length() > 0) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startToast("로그인 되었습니다..");
+                                startCourierPage();
+                            } else {
+                                if(task.getException() != null){
+                                    startToast("잘못입력하셨습니다 다시 입력하여주세요");
+                                }
+                            }
+                        }
+                    });
+        }else {
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
+        }
+
+    }
+    // 토스트 메시지
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private  void startCourierPage(){        Intent intent = new Intent(this, MainFrame.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/fleemarket/SignUp.java
+++ app/src/main/java/com/example/fleemarket/SignUp.java
@@ -1,27 +1,58 @@
 package com.example.fleemarket;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
     private  final int GET_GALLERY_IMAGE = 200;
-    private ImageView imageview;
+    private ImageView imageview,signIn;
+
+    private EditText jPw, jPw1;
+    private TextView pwdText;
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "SingUp";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        jPw = (EditText)findViewById(R.id.jPw);
+        jPw1 = (EditText)findViewById(R.id.jPw1);
+        pwdText = (TextView)findViewById(R.id.pwdText);
+
         imageview=(ImageView)findViewById(R.id.user_image);
+        signIn = (ImageButton)findViewById(R.id.sign_in);
+
         imageview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +65,79 @@ public class SignUp extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.hide();
+
+
+        // 비밀번호 확인
+
+        jPw1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (jPw1.getText().toString().equals(jPw.getText().toString())){
+                    pwdText.setText("일치합니다.");
+                    pwdText.setTextColor(0xAA1e6de0);
+                } else{
+                    pwdText.setTextColor(0xAAef484a);
+                    pwdText.setText("일치하지 않습니다.");
+
+                }
+            }
+        });
+
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+    } // onCreate()
+
+    private  void signUp(){
+        String email = ((EditText)findViewById(R.id.emailaddress)).getText().toString();
+        String password = ((EditText)findViewById(R.id.jPw)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0 ) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //UI
+                                startToast("회원가입이 완료되었습니다..");
+                                LoginActivity();
+                                finish();
+                            } else {
+                                if(task.getException() != null){
+                                    startToast(task.getException().toString());
+                                }
+                            }
+                        }
+                    });
+        }else {
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+    // 토스트 메시지
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    // 회원가입 완료후 메인페이지로 이동
+    private  void LoginActivity(){
+        Intent intent = new Intent(getApplicationContext(),Login.class);
+        intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 
     @Override
