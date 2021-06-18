8b9ad69e4db5809d6d3cc902a19c3a63995b6e4a
kyu0918 <58536057+kyu0918@users.noreply.github.com>
2021-06-18 15:14:59 +0900

    채팅 리스트 개선 // 리사이클러뷰 테두리 구현

--- app/src/main/java/com/example/fleemarket/Chat.java
+++ app/src/main/java/com/example/fleemarket/Chat.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,51 +16,52 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Chat extends Fragment {
 
     private View view;
-    private ArrayList<Dictionary> mArrayList;
-    private CustomAdapter mAdapter;
-    private int count = -1;
+    private CustomAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         view = inflater.inflate(R.layout.activity_chat, container, false);
+        RecyclerView recyclerView1 = view.findViewById(R.id.recyclerView_chat);
 
-        RecyclerView mRecyclerView = view.findViewById(R.id.recyclerview_main_list);
-        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        // recyclerView1의  layoutManger 형식을 지정한다. Grid형식도 설정가능하다.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView1.setLayoutManager(linearLayoutManager);
 
+        adapter = new CustomAdapter();
+        recyclerView1.setAdapter(adapter);
 
-        mArrayList = new ArrayList<>();
-
-        mAdapter = new CustomAdapter( mArrayList);
-        mRecyclerView.setAdapter(mAdapter);
-
-
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
-                mLinearLayoutManager.getOrientation());
-        mRecyclerView.addItemDecoration(dividerItemDecoration);
-
-
-
-        ImageButton buttonInsert =view.findViewById(R.id.button_main_insert);
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                count++;
-
-                Dictionary data = new Dictionary("User"+ count,"메세지" + count);
-
-                //mArrayList.add(0, dict); //RecyclerView의 첫 줄에 삽입
-                mArrayList.add(data); // RecyclerView의 마지막 줄에 삽입
-
-                mAdapter.notifyDataSetChanged();             }
-        });
+        getData();
 
         return view;
+
+
+
+    } //onCreateView
+    private void getData() {
+        List<String> titleList = Arrays.asList("tjdydwns123","별양동핵빤치","sia98");
+        List<String> contentList = Arrays.asList("안녕하세요","채팅이 시작됨.","네고 될까요~~?");
+        List<Integer> rsIdList = Arrays.asList(
+                R.drawable.keyboard,R.drawable.louisvuitton, R.drawable.ipad);
+
+        for (int i=0;i<3;i++) {
+            ChatData data = new ChatData();
+            data.setTitle(titleList.get(i));
+            data.setContent(contentList.get(i));
+            data.setResId(rsIdList.get(i));
+
+            // adapter에 방금 만든 Data 객체를 추가해 넣는다.
+            adapter.addItem(data);
+        }
+
+        // adapter 내용의 값이 변경되었음을 알려준다. 이 함수를 쓰지않으면 data가 노출안된다.
+        // 다만, recyclerView1.setAdapter() 함수가 data를 추가시켜준 뒤에 호출되었다면 정상적으로  data 노출된다.
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/fleemarket/CustomAdapter.java
+++ app/src/main/java/com/example/fleemarket/CustomAdapter.java
@@ -1,5 +1,6 @@
 package com.example.fleemarket;
 
+import android.content.Intent;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,64 +17,74 @@ import java.util.ArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
 
-    private ArrayList<Dictionary> mList;
+    ArrayList<ChatData> dataList = new ArrayList<>();
 
-    public class CustomViewHolder extends RecyclerView.ViewHolder {
-        protected TextView chatUserName;
-        protected TextView chatMessage;
-        protected ImageView chatUserImage;
-        protected ImageView chatProductImage;
-
-
-        public CustomViewHolder(View view) {
-            super(view);
-            this.chatUserName = (TextView) view.findViewById(R.id.chat_user_name);
-            this.chatMessage = (TextView) view.findViewById(R.id.chat_message);
-            this.chatUserImage = (ImageView) view.findViewById(R.id.chat_user_image);
-            this.chatProductImage = (ImageView) view.findViewById(R.id.chat_product_image);
-        }
-    }
-
-
-    public CustomAdapter(ArrayList<Dictionary> list) {
-        this.mList = list;
-    }
-
-
-
+    @NonNull
     @Override
-    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.activity_chat_item_list, viewGroup, false);
+        // item.xml 을 parent ViewGroup 위에 Inflate 시켜 새로운 View를 하나 만든다.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_chat_item_list,parent,false);
 
-        CustomViewHolder viewHolder = new CustomViewHolder(view);
-
-        return viewHolder;
+        // 그리고, 이 view를 바탕으로 ItemViewHolder 객체 생성
+        return new CustomViewHolder(view);
     }
 
-
-
-
+    // ViewHolder 에 각각의 항목들을 바인딩시킨다.
     @Override
-    public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
+    public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int position) {
+        customViewHolder.onBind(dataList.get(position));
+//        itemViewHolder.imageView1.setImageResource(dataList.get(position).getResId());
+//        itemViewHolder.textView1.setText(dataList.get(position).getTitle());
 
-        viewholder.chatUserName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
-        viewholder.chatMessage.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
-
-
-        viewholder.chatUserName.setGravity(Gravity.CENTER);
-        viewholder.chatMessage.setGravity(Gravity.CENTER);
-
-
-
-        viewholder.chatUserName.setText(mList.get(position).getuserName());
-        viewholder.chatMessage.setText(mList.get(position).getMessage());
+        customViewHolder.imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProductInformation.class);
+                intent.putExtra("number", position);
+                intent.putExtra("title", dataList.get(position).getTitle());
+                intent.putExtra("content", dataList.get(position).getContent());
+                v.getContext().startActivity(intent);
+                Toast.makeText(v.getContext(), "채팅방에 접속하셨습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return (null != mList ? mList.size() : 0);
+        return dataList.size();
     }
 
+    // Data 객체(아이템) 을 하나씩 추가시킨다.
+    public void addItem(ChatData data) {
+        dataList.add(data);
+    }
+
+
+    // RecyclerView 의 ViewHolder 만든다.
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView1;
+        private TextView textView2;
+        private ImageView imageView1;
+        private ImageView imageView2;
+
+        CustomViewHolder(@NonNull View customView) {
+            super(customView);
+
+            textView1 = customView.findViewById(R.id.chat_user_name);
+            textView2 = customView.findViewById(R.id.chat_message);
+            imageView1 = customView.findViewById(R.id.chat_user_image);
+            imageView2 = customView.findViewById(R.id.chat_product_image);
+
+        }
+
+        // 실제 데이터들을 1:1 대응하여 각각의 내부뷰에 바인딩시킨다.
+        void onBind(ChatData data) {
+            textView1.setText(data.getTitle());
+            textView2.setText(data.getContent());
+            imageView1.setImageResource(data.getResId());
+
+        }
+    }
 }
